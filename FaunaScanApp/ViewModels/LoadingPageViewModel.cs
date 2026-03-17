d72076655e2d05a37fded35300deec24ae5bccf4
Tomspns <tom.spaans@saintmichelannecy.fr>
2026-03-17 14:47:30 +0100

    Revert "Initial commit FaunaScan app"

--- FaunaScanApp/ViewModels/LoadingPageViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace FaunaScanApp.ViewModels;
-
-public partial class LoadingPageViewModel : ObservableObject
-{
-    [ObservableProperty]
-    private double progress;
-}
